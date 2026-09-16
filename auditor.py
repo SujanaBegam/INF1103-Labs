ac3df7955e57e7a9b272bdaede21a38c9a278e01
SujanaBegam <2605696@sit.singaporetech.edu.sg>
2026-09-16 19:45:41 +0800

    Task 1

---
+++ auditor.py
@@ -0,0 +1,45 @@
+inventory = 0
+
+failed_entries = 0
+
+
+
+while True:
+
+    quantity = input("Enter stock quantity: ")
+
+
+
+    if quantity == "quit":
+
+        break
+
+
+
+    if not quantity.isdigit():
+
+        print("Invalid input")
+
+        failed_entries += 1
+
+        continue
+
+
+
+    quantity = int(quantity)
+
+
+
+    inventory += quantity
+
+
+
+    if inventory > 500:
+
+        print("OVERSTOCK ALERT!: Inventory exceeds 500 units.")
+
+        break
+
+
+print("Total Units Processed:", inventory)
+print("Failed/Rejected Entries:", failed_entries)
